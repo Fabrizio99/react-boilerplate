3ab30ca36a73fe21f41c0588d0be08994f3e70f3
Fabrizio <fabrizio.condori.guzman@gmail.com>
2021-12-05 13:03:00 -0500

    format with prettier before commiting

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,23 @@ import React from 'react'
 import './App.css'
 import logo from './logo.svg'
 
-const App = () => {
-	console.log('Rendering App')
-	console.log('Rendering App')
-	console.log('Rendering App')
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	)
-}
+const App = () => (
+	<div className='App'>
+		<header className='App-header'>
+			<img src={logo} className='App-logo' alt='logo' />
+			<p>
+				Edit <code>src/App.tsx</code> and save to reload.
+			</p>
+			<a
+				className='App-link'
+				href='https://reactjs.org'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				Learn React
+			</a>
+		</header>
+	</div>
+)
 
 export default App
