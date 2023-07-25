1703244c087beb6d723a45b33ab8d68c61735f7a
Fabrizio99 <fabrizio.condori.guzman@gmail.com>
2023-07-24 23:19:31 -0500

    install and configure vitest

---
+++ src/file.test.ts
@@ -0,0 +1,5 @@
+describe('test smth', () => {
+	it('should be true', () => {
+		expect(true).toBeTruthy()
+	})
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import * as path from "path";
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -8,4 +8,8 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
+  test: {
+    globals: true,
+    environment: 'happy-dom'
+  }
 })
